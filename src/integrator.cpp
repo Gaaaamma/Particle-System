6de7188abfaa809b8e86bec981ed8123577d737a
Gaaaamma <ytching552022@gmail.com>
2022-03-20 11:14:54 +0800

    Add: midPoint (not sure)

--- src/integrator.cpp
+++ src/integrator.cpp
@@ -15,7 +15,6 @@ void ExplicitEuler::integrate(const std::vector<Particles *> &particles, std::fu
     // (1) p point to cloth.particle  (2) p point to spheres.particle
     // Main idea is to update its velocity & position
     // formular: x(t+h) = x(t) + h * f(x,t) 
-    // Decide to handle each vector 
     p->position() += deltaTime * p->velocity();
     p->velocity() += deltaTime * p->acceleration();
   }
@@ -42,7 +41,50 @@ void MidpointEuler::integrate(const std::vector<Particles *> &particles,
   // Note:
   //   1. Use simulateOneStep with modified position and velocity to get Xn+1.
 
-  // Write code here!
+  // (1) p point to cloth.particle  (2) p point to spheres.particle
+  // Main idea is to update its velocity & position
+  // formular: x(t+h) = x(t) + h * velocity_future
+
+  // Main idea: How to get velocity_future?
+  // 0. Save original particles' data. (position / velocity / acceleration)
+  Eigen::Matrix4Xf cl_origin_position = particles[0]->position();
+  Eigen::Matrix4Xf cl_origin_velocity = particles[0]->velocity();
+  Eigen::Matrix4Xf cl_origin_acceleration = particles[0]->acceleration();
+
+  Eigen::Matrix4Xf sp_origin_position = particles[1]->position();
+  Eigen::Matrix4Xf sp_origin_velocity = particles[1]->velocity();
+  Eigen::Matrix4Xf sp_origin_acceleration = particles[1]->acceleration();
+
+  // 1. move the position&velocity of particle to future position
+  particles[0]->position() += deltaTime / 2 * particles[0]->velocity();
+  particles[1]->position() += deltaTime / 2 * particles[1]->velocity();
+  particles[0]->velocity() += deltaTime / 2 * particles[0]->acceleration();
+  particles[1]->velocity() += deltaTime / 2 * particles[1]->acceleration();
+
+  // 2. modify the status of particle via simulateOneStep
+  simulateOneStep();
+
+  // 3. get  velocity_future acceleration_future
+  Eigen::Matrix4Xf cl_future_velocity = particles[0]->velocity();
+  Eigen::Matrix4Xf sp_future_velocity = particles[1]->velocity();
+  Eigen::Matrix4Xf cl_future_acceleration = particles[0]->acceleration();
+  Eigen::Matrix4Xf sp_future_acceleration = particles[1]->acceleration();
+
+  // 4. Recover particles' status
+  particles[0]->position() = cl_origin_position;
+  particles[0]->velocity() = cl_origin_velocity;
+  particles[0]->acceleration() = cl_origin_acceleration;
+
+  particles[1]->position() = sp_origin_position;
+  particles[1]->velocity() = sp_origin_velocity;
+  particles[1]->acceleration() = sp_origin_acceleration;
+
+  // 5. Refined position & velocity
+  particles[0]->position() += deltaTime * cl_future_velocity;
+  particles[0]->velocity() += deltaTime * cl_future_acceleration;
+
+  particles[1]->position() += deltaTime * sp_future_velocity;
+  particles[1]->velocity() += deltaTime * sp_future_acceleration;
 }
 
 void RungeKuttaFourth::integrate(const std::vector<Particles *> &particles,
